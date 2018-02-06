0448f74e8b0eb945f575c7434503f5259ec942e6
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-02-05 21:37:01 -0800

    some changes added

--- script.js
+++ script.js
@@ -9,21 +9,33 @@ $(document).ready(function(){
             type: "GET",
             dataType: "jsonp",
             success:function(data){
+                var widget= show(data);
+                $("#show").html(widget);
+                $("#city").val("");
+
                console.log(data);
             }
-
-
-
-
-            })
-
-
+        });
         } else {
             $("#error").html("Field can not be empty")
         }
 
     });
-
-
-
 });
+function show(data){
+    return "<h3><strong>Weather</strong>: "+ data.weather[0].main+"</h3>"+
+           "<h3><strong>Description</strong>: "+ data.weather[0].description+"</h3>"+
+           "<h3><strong>Temperature</strong>: "+ data.weather[0].main.temp+"</h3>"+
+           "<h3><strong>Pressure</strong>: "+ data.main.pressure+"</h3>"+
+           "<h3><strong>Humidity</strong>: "+ data.main.humidity+"</h3>"+
+           "<h3><strong>Temp Max</strong>: "+ data.main.temp_max+"</h3>"+
+           "<h3><strong>Temp Min</strong>: "+ data.main.temp_min+"</h3>"+
+           "<h3><strong>Wind speed</strong>: "+ data.wind.speed+"</h3>"+
+           "<h3><strong>Wind direction</strong>: "+ data.wind.deg+"</h3>";
+
+
+
+
+
+
+};
