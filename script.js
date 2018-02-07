681487020376e88b2c400457f20541881a9f2cf1
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-02-07 12:33:07 -0800

    background color made yellow

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
                 console.log(data);
                 var widget= show(data);
                 $("#show").html(widget);
-                $(".city").html(data.name);
+                $(".city").html(data.name+", "+data.sys.country);
             }
         });
         } else {
@@ -20,13 +20,15 @@ $(document).ready(function(){
     });
 });
 function show(data){
-    return "<h5><strong>Weather</strong>: "+data.weather[0].main+"</h5>"+
+    return "<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'/>"+
+           "<h5><strong>Weather</strong>: "+data.weather[0].main+"</h5>"+
            "<h5><strong>Description</strong>: "+data.weather[0].description+"</h5>"+
            "<h5><strong>Temperature</strong>: "+data.main.temp+"&#8457</h5>"+
-           "<h5><strong>Pressure</strong>: "+data.main.pressure+"</h5>"+
-           "<h5><strong>Humidity</strong>: "+data.main.humidity+"</h5>"+
+           "<h5><strong>Pressure</strong>: "+data.main.pressure+"hPa</h5>"+
+           "<h5><strong>Humidity</strong>: "+data.main.humidity+"%</h5>"+
            "<h5><strong>Temp Max</strong>: "+data.main.temp_max+"&#8457</h5>"+
            "<h5><strong>Temp Min</strong>: "+data.main.temp_min+"&#8457</h5>"+
-           "<h5><strong>Wind speed</strong>: "+data.wind.speed+"</h5>"+
-           "<h5><strong>Wind direction</strong>: "+data.wind.deg+"</h5>";
+           "<h5><strong>Wind speed</strong>: "+data.wind.speed+"m/sec</h5>"+
+           "<h5><strong>Wind direction</strong>: "+data.wind.deg+"&#176</h5>";
+           "<span>Icon</span>: "+data.weather[0].icon+"</span>"
 };
