2bce913dadf5ea564680e179e0d5db50a8b733df
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-02-19 12:53:42 -0800

    code copied from codePen

--- script.js
+++ script.js
@@ -1,33 +1,96 @@
-
 $(document).ready(function(){
-    $("#submit").on("click", function(){
+    $("#reset").click(function(){
+        location.reload();
+    });
+    $("#submit").click(function(){
         var city = $("#city").val();
-        if (city != "") {
+        if (city != ""){
             $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+"&appid=8f236e524e673e139b0f6739b81a6eb4",
-            type: "GET",
-            dataType: "jsonp",
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}
+                  &units=imperial&appid=8f236e524e673e139b0f6739b81a6eb4`,
+            data: "",
+            type: "get",
+            dataType: "json",
             success:function(data){
                 console.log(data);
-                var widget= show(data);
+                var widget = show(data);
                 $("#show").html(widget);
-                $(".city").html(data.name+", "+data.sys.country);
-            }
+                $(".conversion").css("display", "block");
+            },
+            error: function(response){
+                $('#show').html("Either city name is incorrect or data not available.");
+          },
+            beforesend: "",
+            complete: ""
         });
         } else {
-            $("#error").html("City name can not be empty")
+            $("#show").html("<h3 style='color:red'><i>City name empty<i></h3>")
         }
     });
 });
+
 function show(data){
-    return "<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'/>"+
-           "<h5><strong>Weather</strong>: "+data.weather[0].main+"</h5>"+
-           "<h5><strong>Description</strong>: "+data.weather[0].description+"</h5>"+
-           "<h5><strong>Temperature</strong>: "+data.main.temp+"&#8457</h5>"+
-           "<h5><strong>Pressure</strong>: "+data.main.pressure+"hPa</h5>"+
-           "<h5><strong>Humidity</strong>: "+data.main.humidity+"%</h5>"+
-           "<h5><strong>Temp Max</strong>: "+data.main.temp_max+"&#8457</h5>"+
-           "<h5><strong>Temp Min</strong>: "+data.main.temp_min+"&#8457</h5>"+
-           "<h5><strong>Wind speed</strong>: "+data.wind.speed+"m/sec</h5>"+
-           "<h5><strong>Wind direction</strong>: "+data.wind.deg+"&#176</h5>";
-};
+    return (
+       `<img style="background-color:skyblue" src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'/><br>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Description: ${data.weather[0].description}</p>
+        <p>Temperature: ${data.main.temp} &#8457</p>
+        <p>Pressure: ${data.main.pressure} hPa</p>
+        <p>Humidity: ${data.main.humidity} %</p>
+        <p>Temp Max: ${data.main.temp_max} &#8457</p>
+        <p>Temp Min: ${data.main.temp_min} &#8457</p>
+        <p>Wind speed: ${data.wind.speed} mile/hr</p>
+        <p>Wind direction: ${data.wind.deg} &#176</p>`
+        // <p style="color:blue">[ ${data.name}, ${data.sys.country}]<p>
+        );
+    }
+
+/*Metric:-
+$(document).ready(function(){
+    $("#reset").click(function(){
+        location.reload();
+    });
+    $(".conversion").click(function(){
+        var city = $("#city").val();
+        if (city != ""){
+            $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}
+                  &units=metric&appid=8f236e524e673e139b0f6739b81a6eb4`,
+            data: "",
+            type: "get",
+            dataType: "json",
+            success:function(data){
+                console.log(data);
+                var widget = show(data);
+                $("#show").html(widget);
+            },
+            error: function(response){
+                $('#show').html("Either city name is incorrect or data not available.");
+          },
+            beforesend: "",
+            complete: ""
+        });
+        } else {
+            $("#show").html("<h3 style='color:red'><i>City name empty<i></h3>")
+        }
+    });
+});
+
+function show(data){
+    return (
+       `<img style="background-color:skyblue" src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'/><br>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Description: ${data.weather[0].description}</p>
+        <p>Temperature: ${data.main.temp} &#8451</p>
+        <p>Pressure: ${data.main.pressure} hPa</p>
+        <p>Humidity: ${data.main.humidity} %</p>
+        <p>Temp Max: ${data.main.temp_max} &#8451</p>
+        <p>Temp Min: ${data.main.temp_min} &#8451</p>
+        <p>Wind speed: ${data.wind.speed} km/hr</p>
+        <p>Wind direction: ${data.wind.deg} &#176</p>
+        <p style="color:blue">[ ${data.name}, ${data.sys.country}]<p>`
+        );
+    }
+
+
+*/
